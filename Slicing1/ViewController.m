474cffeb6cbf00d1b48da0a5dc649c832afbbb96
Santosh Vishwakarma <vish_santosh85@yahoo.co.in>
2016-11-27 10:43:43 +0530

    Added button and its bg resource in asset catalogue

--- Slicing1/ViewController.m
+++ Slicing1/ViewController.m
@@ -19,6 +19,19 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)buttonAction:(id)sender {
+    
+    UIAlertController *alert = [[UIAlertController alloc] init];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
+    
+    [alert addAction:okAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.

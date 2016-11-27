56c5ec0112673de53f85e3be9ddab1c879cf5358
Santosh Vishwakarma <vish_santosh85@yahoo.co.in>
2016-11-27 10:22:27 +0530

    Initial Commit

---
+++ Slicing1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Slicing1
+//
+//  Created by Santosh Vishwakarma on 27/11/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Slicing1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Slicing1
+//
+//  Created by Santosh Vishwakarma on 27/11/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
